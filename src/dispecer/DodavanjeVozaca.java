356a3b3a9a57aed8ea8587b9fcaf10dfda8cf184
Petar <78502458+ppetar33@users.noreply.github.com>
2021-04-04 17:03:00 +0200

    jad i cemer

---
+++ src/dispecer/DodavanjeVozaca.java
@@ -0,0 +1,10 @@
+package dispecer;
+
+public class DodavanjeVozaca {
+
+	public static void main(String[] args){
+		
+		// prilikom dodavanja vozača, vozaču je moguće dodeliti jedan od slobodnih automobila, ako postoji)
+	}
+
+}
--- src/dispecer/PrikazVozaca.java
+++ src/dispecer/PrikazVozaca.java
@@ -1,49 +1,53 @@
 package dispecer;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+
 
 public class PrikazVozaca {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		
-		String[] words = readArray("src/fajlovi/korisnici.txt");
-		for(int i = 0; i < words.length ; i++) {
-			if(words[i].contains("DISPECAR")) {
-				System.out.println(words[i]);
-			}
-		}
-		// zbog razmaka ne prikazuje podatke
-	
+		ucitajVozaca();
+		
 	}
-	
-	public static String[] readArray(String file) {
-		
-		int ctr = 0;
+
+	public static void ucitajVozaca() {
 		try {
-			
-			Scanner s1 = new Scanner(new File(file));
-
-			while(s1.hasNextLine()) {
-				ctr = ctr + 1;
-				s1.next();
+			File korisniciFajl = new File("src/fajlovi/korisnici.txt");
+			BufferedReader br = new BufferedReader(new FileReader(korisniciFajl));
+			String line = null;
+			while((line = br.readLine()) != null) {
+				String[] split = line.split("\\,");
+				String korisnickoIme = split[0];
+				String lozinka = split[1];
+				String ime = split[2];
+				String prezime = split[3];
+				String jmbgString = split[4];
+				double jmbg = Double.parseDouble(jmbgString);
+				String adresa = split[5];
+				String pol = split[6];
+				String brojTelefonaString = split[7];
+				int brojTelefona = Integer.parseInt(brojTelefonaString);
+				String tip = split[10];
+				if(tip.equals("MUSTERIJA")) {
+					String prazan01 = split[8];
+					String prazan02 = split[9];
+				}else if(tip.equals("DISPECAR")) {
+					String plataString = split[8];
+					int plata = Integer.parseInt(plataString);
+					String brojPozivnogTelefonaString = split[9];
+					int brojPozivnogTelefona = Integer.parseInt(brojPozivnogTelefonaString);
+				}else if(tip.equals("VOZAC")) {
+					String plataString = split[8];
+					int plata = Integer.parseInt(plataString);
+					String brojKarticeString = split[9];
+					int brojKartice = Integer.parseInt(brojKarticeString);
+					String automobil = split[10];
+				}
 			}
-			
-			String[] words = new String[ctr];
-
-			Scanner s2 = new Scanner(new File(file));
-			
-			for(int i = 0; i < ctr; i++) {
-				words[i] = s2.next();
-			}
-			
-			return words;
-			
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		return null;
 	}
 }
