b5ea37b12aba40cd0cedf5241d576914e93321de
Petar <78502458+ppetar33@users.noreply.github.com>
2021-04-05 19:09:52 +0200

    changed

--- src/dispecer/PrikazVozaca.java
+++ src/dispecer/PrikazVozaca.java
@@ -54,4 +54,6 @@ public class PrikazVozaca {
 			e.printStackTrace();
 		}
 	}
+	
+	
 }
--- src/ucitavanje/Ucitavanje.java
+++ src/ucitavanje/Ucitavanje.java
@@ -19,29 +19,46 @@ public class Ucitavanje {
     }
 
     public void ucitajZaposlene(String imeFajla) {
-        try {
-            File prodavciFile = new File("src/fajlovi/" + imeFajla);
-            BufferedReader br = new BufferedReader(new FileReader(prodavciFile));
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                String[] split = line.split(",");
-                String korisnickoIme = split[0];
-                String lozinka = split[1];
-                String ime = split[2];
-                String prezime = split[3];
-                String jmbg = split[4];
-//                int jmbg = Integer.parseInt(split[4]);
-                String adresa = split[5];
-                String pol = split[6];
-                String brojTelefona = split[7];
-//                int brojTelefona = Integer.parseInt(brojTelefonaString);
-                Musterija musterija = new Musterija(korisnickoIme, lozinka, ime, prezime, jmbg, adresa, pol, brojTelefona);
-                musterije.add(musterija);
-            }
-            br.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    	try {
+			File korisniciFajl = new File("src/fajlovi/korisnici.txt");
+			BufferedReader br = new BufferedReader(new FileReader(korisniciFajl));
+			String line = null;
+			while((line = br.readLine()) != null) {
+				String[] split = line.split("\\,");
+				String korisnickoIme = split[0];
+				String lozinka = split[1];
+				String ime = split[2];
+				String prezime = split[3];
+				String jmbgString = split[4];
+				double jmbg = Double.parseDouble(jmbgString);
+				String adresa = split[5];
+				String pol = split[6];
+				String brojTelefonaString = split[7];
+				int brojTelefona = Integer.parseInt(brojTelefonaString);
+				String tip = split[11];
+				if(tip.equals("MUSTERIJA")) {
+					String prazan01 = split[8];
+					String prazan02 = split[9];
+					String prazan03 = split[10];
+				}else if(tip.equals("DISPECAR")) {
+					String plataString = split[8];
+					int plata = Integer.parseInt(plataString);
+					String brojPozivnogTelefonaString = split[9];
+					int brojPozivnogTelefona = Integer.parseInt(brojPozivnogTelefonaString);
+					String deoGradaNaKojemRadi = split[10];
+				}else if(tip.equals("VOZAC")) {
+					String plataString = split[8];
+					int plata = Integer.parseInt(plataString);
+					String brojKarticeString = split[9];
+					int brojKartice = Integer.parseInt(brojKarticeString);
+					String automobil = split[10];
+				}
+				
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
     public Musterija login(String korisnickoIme, String lozinka) {
         for (Musterija musterija : musterije) {
