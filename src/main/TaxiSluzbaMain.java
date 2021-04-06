60cf018c971d32ab2051985c8f0fd32bc30d2d78
Petar <78502458+ppetar33@users.noreply.github.com>
2021-04-06 18:39:15 +0200

    "test"

--- src/main/TaxiSluzbaMain.java
+++ src/main/TaxiSluzbaMain.java
@@ -6,7 +6,7 @@ import ucitavanje.Ucitavanje;
 public class TaxiSluzbaMain {
 	
     private static String KORISNICI_FAJL = "korisnici.txt";
-    // da
+
 	public static void main(String[] args){
 		
         Ucitavanje ucitavanje = new Ucitavanje();
