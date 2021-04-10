1b9c8620f76c27146b1a2705bdeb9ad322fec181
Vladimir Djurdjevic <vladimir.djurdjevic411@gmail.com>
2021-04-10 22:39:06 +0200

    Napravio sam entitet Voznja.java

--- src/automobili/Voznja.java
+++ src/automobili/Voznja.java
@@ -2,18 +2,103 @@ package automobili;
 
 public class Voznja {
 
-    /*
-        Osnovni entitet u sklopu poslovanja taksi službe je vožnja. Za svaku vožnju se evidentiraju sledeći podaci:
-            Datum i vreme porudžbine, adresa polaska, adresa destinacije, mušterija koja je naručila vožnju,
-            vozač koji je preuzeo porudžbinu, broj km pređenih u vožnji, trajanje vožnje i status vožnje.
-            Status vožnje može biti KREIRANA, KREIRANA-NA ČEKANJU, DODELJENA, PRIHV AĆENA, ZAVRŠENA, ODBIJENA.
-            Vožnje je moguće poručiti na dva načina:
-                o Telefonskim pozivom: Kada se kreira ova vožnja njen inicijalni status je KREIRANA. Nakon što
-                dispečer vožnju dodeli određenom vozaču njen status postaje DODELJENA.
-                o Putem aplikacije: Za sve porudžbine preko aplikacije potrebno je evidentirati tekstualnu
-                napomenu koju unose korisnici. Kada se kreira ova vožnja njen inicijalni status je
-                KREIRANA-NA ČEKANJU.
-     */
+    private String datumIvremePorudzbine;
+    private String adresaPolaska;
+    private String adresaDestinacije;
+    private String musterija;
+    private String vozac;
+    private double brojKMpredjenih;
+    private double trajanjVoznje;
+    private StatusVoznje statusVoznje;
 
+    public Voznja(){}
 
+    public Voznja(String datumIvremePorudzbine, String adresaPolaska, String adresaDestinacije, String musterija, String vozac, double brojKMpredjenih, double trajanjVoznje, StatusVoznje statusVoznje){
+        this.datumIvremePorudzbine = datumIvremePorudzbine;
+        this.adresaPolaska = adresaPolaska;
+        this.adresaDestinacije = adresaDestinacije;
+        this.musterija = musterija;
+        this.vozac = vozac;
+        this.brojKMpredjenih = brojKMpredjenih;
+        this.trajanjVoznje = trajanjVoznje;
+        this.statusVoznje = statusVoznje;
+    }
+
+    public String getDatumIvremePorudzbine() {
+        return datumIvremePorudzbine;
+    }
+
+    public void setDatumIvremePorudzbine(String datumIvremePorudzbine) {
+        this.datumIvremePorudzbine = datumIvremePorudzbine;
+    }
+
+    public String getAdresaPolaska() {
+        return adresaPolaska;
+    }
+
+    public void setAdresaPolaska(String adresaPolaska) {
+        this.adresaPolaska = adresaPolaska;
+    }
+
+    public String getAdresaDestinacije() {
+        return adresaDestinacije;
+    }
+
+    public void setAdresaDestinacije(String adresaDestinacije) {
+        this.adresaDestinacije = adresaDestinacije;
+    }
+
+    public String getMusterija() {
+        return musterija;
+    }
+
+    public void setMusterija(String musterija) {
+        this.musterija = musterija;
+    }
+
+    public String getVozac() {
+        return vozac;
+    }
+
+    public void setVozac(String vozac) {
+        this.vozac = vozac;
+    }
+
+    public double getBrojKMpredjenih() {
+        return brojKMpredjenih;
+    }
+
+    public void setBrojKMpredjenih(double brojKMpredjenih) {
+        this.brojKMpredjenih = brojKMpredjenih;
+    }
+
+    public double getTrajanjVoznje() {
+        return trajanjVoznje;
+    }
+
+    public void setTrajanjVoznje(double trajanjVoznje) {
+        this.trajanjVoznje = trajanjVoznje;
+    }
+
+    public StatusVoznje getStatusVoznje() {
+        return statusVoznje;
+    }
+
+    public void setStatusVoznje(StatusVoznje statusVoznje) {
+        this.statusVoznje = statusVoznje;
+    }
+
+    @Override
+    public String toString() {
+        return "Voznja{" +
+                "datumIvremePorudzbine='" + datumIvremePorudzbine + '\'' +
+                ", adresaPolaska='" + adresaPolaska + '\'' +
+                ", adresaDestinacije='" + adresaDestinacije + '\'' +
+                ", musterija='" + musterija + '\'' +
+                ", vozac='" + vozac + '\'' +
+                ", brojKMpredjenih=" + brojKMpredjenih +
+                ", trajanjVoznje=" + trajanjVoznje +
+                ", statusVoznje=" + statusVoznje +
+                '}';
+    }
 }
