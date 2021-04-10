4a1a9eb81f274f7c2e0ec90a238121b9b9b24628
Petar <78502458+ppetar33@users.noreply.github.com>
2021-04-10 23:03:11 +0200

    changed

--- src/dispecer/BrisanjeVozaca.java
+++ src/dispecer/BrisanjeVozaca.java
@@ -1,4 +1,10 @@
 package dispecer;
 
 public class BrisanjeVozaca {
+    /*
+        Prilikom brisanja, svi entiteti se brišu logički što znači da podaci o njima treba da ostanu u
+        datotekama ali obrisani entiteti (niti njihovi povezani entiteti) ne treba da se vide u sistemu,
+        osim u situacijama kada to nema smisla. (Primer: brisanje vozača ne mora da podrazumeva brisanje
+        automobila)
+    */
 }
