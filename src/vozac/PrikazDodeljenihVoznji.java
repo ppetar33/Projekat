23270ecf16a016774c88e84d3bbe24fddbbb5039
Petar <78502458+ppetar33@users.noreply.github.com>
2021-04-10 22:36:53 +0200

    new files

---
+++ src/vozac/PrikazDodeljenihVoznji.java
@@ -0,0 +1,9 @@
+package vozac;
+
+public class PrikazDodeljenihVoznji {
+    /*
+        Prikaz dodeljenih vožnji (vožnje kreirane putem telefonskog poziva, koje je dispečer dodelio vozaču),
+        vozač ima mogućnost da prihvati ili odbije vožnju. Prihvatanjem vožnja menja status iz DODELJENA u
+        PRIHVAĆENA, dok odbijanjem vožnja menja status iz DODELJENA U ODBIJENA.
+    */
+}
