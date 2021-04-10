1b4875d94c335f424d41d27b9591c9610f6ca43e
Petar <78502458+ppetar33@users.noreply.github.com>
2021-04-10 22:42:15 +0200

    comment to class voznja

--- src/automobili/Voznja.java
+++ src/automobili/Voznja.java
@@ -2,6 +2,19 @@ package automobili;
 
 public class Voznja {
 
+    /*
+        Osnovni entitet u sklopu poslovanja taksi službe je vožnja. Za svaku vožnju se evidentiraju sledeći podaci:
+            Datum i vreme porudžbine, adresa polaska, adresa destinacije, mušterija koja je naručila vožnju,
+            vozač koji je preuzeo porudžbinu, broj km pređenih u vožnji, trajanje vožnje i status vožnje.
+            Status vožnje može biti KREIRANA, KREIRANA-NA ČEKANJU, DODELJENA, PRIHV AĆENA, ZAVRŠENA, ODBIJENA.
+            Vožnje je moguće poručiti na dva načina:
+                1. o Telefonskim pozivom: Kada se kreira ova vožnja njen inicijalni status je KREIRANA. Nakon što
+                dispečer vožnju dodeli određenom vozaču njen status postaje DODELJENA.
+                2. o Putem aplikacije: Za sve porudžbine preko aplikacije potrebno je evidentirati tekstualnu
+                napomenu koju unose korisnici. Kada se kreira ova vožnja njen inicijalni status je
+                KREIRANA-NA ČEKANJU.
+    */
+
     private String datumIvremePorudzbine;
     private String adresaPolaska;
     private String adresaDestinacije;
